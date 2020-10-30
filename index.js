f7a9a4be7c18c0cab2edc4a6ae03583511a89594
Kole Gasior <jkole822@gmail.com>
2020-10-30 18:35:21 -0400

    pseudocode and structure html with bootstrap

---
+++ index.js
@@ -0,0 +1,13 @@
+// Need to dynamically render each section of html one at a time
+
+// Write up eight questions, each with four multiple choice options
+// that will dynamically be rendered upon user interaction.
+
+// After user completes eight questions, the score will be calculated based
+// on user input and displayed on a results screen where the user will be able
+// to submit their initials with their score which will be saved to an object.
+
+// After submitting their initials, the highscore section will be rendered
+// which will show all previous scores ranked in order with the corresponding
+// initials. The user will then have then option to clear the highscore list and
+// go back to the start of the quiz.
